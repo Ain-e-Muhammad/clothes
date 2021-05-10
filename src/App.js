9631fda639ad6dfb5b85326c4517447a418a4ad5
Ain <ain.e.muhammad@tintash.com>
2021-05-10 15:40:28 +0500

    Added header to all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component"
 import shopPage from "./pages/shoppage/shop.component"
 import {Switch, Route} from 'react-router-dom'
-
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={shopPage}/>   
