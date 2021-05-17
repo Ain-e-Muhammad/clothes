0ad541c5a99c511807d390dac635546f2a1b1f83
Ain <ain.e.muhammad@tintash.com>
2021-05-17 10:07:09 +0500

    Added the cart icon and cart dropdown, used redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component"
 import shopPage from "./pages/shoppage/shop.component"
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
@@ -46,12 +46,16 @@ class  App extends Component{
         <Switch>
           <Route exact path='/' component={Homepage}/>
           <Route exact path='/shop' component={shopPage}/>   
-          <Route exact path='/signin' component={SignInAndSignUp}/>
+          <Route exact path='/signin' render = { () => this.props.currentUser ? (<Redirect to ='/'/>) : (<SignInAndSignUp/>) }/>
         </Switch>
       </div>
     )
   }
 }
+
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => {
@@ -59,4 +63,4 @@ const mapDispatchToProps = (dispatch) => ({
   }
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,16 @@
+const Initial_State = {
+    hidden : true
+}
+
+const cartReducer = (state = Initial_State, action) => {
+    switch(action.type){
+        case 'TOGGLE_CART_HIDDEN':
+            return{
+                ...state,
+                hidden : !state.hidden
+            }
+        default:
+            return state
+    }
+}
+export default cartReducer
