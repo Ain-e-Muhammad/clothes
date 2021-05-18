cc39b5c72f978a2a47b13923c8931a7a66ce8a8c
Ain <ain.e.muhammad@tintash.com>
2021-05-18 12:09:03 +0500

    Added session storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
 import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './root-reducer'
+import {persistStore} from 'redux-persist'
+
 
 const middlewares = [logger]
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(
+    rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-export default store
+export const persistor= persistStore(store)
+
+// export default store
